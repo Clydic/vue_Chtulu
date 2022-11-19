50e1e56722909da71bef5832e72668bdcc18d461
Cédric Paimblanc <cedric.du.50@gmail.com>
2022-11-19 20:10:47 +0100

    Initialisation

---
+++ src/views/HomeView2.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+  </header>
+  <main>
+  </main>
+</template>
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+const { createApp } = Vue;
+  createApp({
+    data() {
+      return {
+        title : 'Chtulu'
+      }
+    },
+  })
+</script>
